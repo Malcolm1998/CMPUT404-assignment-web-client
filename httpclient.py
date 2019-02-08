4e02c55f21cf939f11e976e1dd4ef06b1bfd2570
Malcolm <mimacart@ualberta.ca>
2019-02-08 10:02:02 -0700

    Done assignment

--- httpclient.py
+++ httpclient.py
@@ -36,24 +36,27 @@ class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
+        self.socket = None
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
-
-    def get_headers(self,data):
+        if data.find("HTTP/1.0") >= 0:
+            return int(data.split("\r\n")[0].replace("HTTP/1.0 ", "")[:3])
+        elif data.find("HTTP/1.1") >= 0:
+            return int(data.split("\r\n")[0].replace("HTTP/1.1 ", "")[:3])
         return None
 
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[-1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
         
     def close(self):
-        self.socket.close()
+        if self.socket is not None:
+            self.socket.close()
 
     # read everything from the socket
     def recvall(self, sock):
@@ -68,13 +71,74 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        host_and_port = url.split("/")[2]
+        host = host_and_port.split(":")[0]
+        try:
+            port = int(host_and_port.split(":")[1])
+        except IndexError:
+            port = 80
+        path = "/"+"/".join(url.split("/")[3:])
+
+        request = "GET "+path+" HTTP/1.1\r\nHost: "+host+"\r\n\r\n"
+        response = ""
+
+        try:
+            self.connect(host, port)
+            self.sendall(request)
+
+            while 1:
+                chunk = self.socket.recv(2048).decode("utf-8")
+                if chunk != "":
+                    response += chunk
+                else:
+                    break
+        finally:
+                self.close()
+
+        print(response)
+        code = self.get_code(response)
+        if code != 404:
+            body = self.get_body(response)
+        else:
+            body = None
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
+        host_and_port = url.split("/")[2]
+        host = host_and_port.split(":")[0]
+        port = int(host_and_port.split(":")[1])
+        path = "/" + "/".join(url.split("/")[3:])
+
         body = ""
+        if args is not None:
+            for arg in args:
+                body += arg+"="+args[arg]+"&"
+            body = body[:-1]
+        content_length = str(len(body))
+
+        # Note: Content-Length must equal the number of characters in body
+        request = "POST "+path+" HTTP/1.1\r\nHost: "+host+"\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: "+content_length+"\r\n\r\n"+body
+        response = ""
+
+        try:
+            self.connect(host, port)
+            self.sendall(request)
+
+            while 1:
+                chunk = self.socket.recv(2048).decode("utf-8")
+                if chunk != "":
+                    response += chunk
+                else:
+                    break
+        finally:
+                self.close()
+
+        print(response)
+        code = self.get_code(response)
+        if code != 404:
+            body = self.get_body(response)
+        else:
+            body = None
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -90,6 +154,9 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        test = client.command( sys.argv[2], sys.argv[1] )
+        print(test)
     else:
-        print(client.command( sys.argv[1] ))
+        test = client.command( sys.argv[1] )
+        print(test)
+    print("test")
